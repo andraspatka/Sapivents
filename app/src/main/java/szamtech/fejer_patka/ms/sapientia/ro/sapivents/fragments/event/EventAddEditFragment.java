a76d23a928c5c85b036438fb49414ff79bf66ceb
Patka Zsolt-Andras <andras.patka@gmail.com>
2018-11-17 20:23:25 +0200

    Added ImagePicker functionality

--- app/src/main/java/szamtech/fejer_patka/ms/sapientia/ro/sapivents/fragments/event/EventAddEditFragment.java
+++ app/src/main/java/szamtech/fejer_patka/ms/sapientia/ro/sapivents/fragments/event/EventAddEditFragment.java
@@ -1,5 +1,7 @@
 package szamtech.fejer_patka.ms.sapientia.ro.sapivents.fragments.event;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -13,6 +15,8 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
+import java.io.FileNotFoundException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -20,6 +24,8 @@ import szamtech.fejer_patka.ms.sapientia.ro.sapivents.R;
 import szamtech.fejer_patka.ms.sapientia.ro.sapivents.beans.Event;
 import szamtech.fejer_patka.ms.sapientia.ro.sapivents.utils.EventPrefUtil;
 import szamtech.fejer_patka.ms.sapientia.ro.sapivents.utils.FragmentNavigationUtil;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Fragment for adding/editing event details
@@ -34,6 +40,7 @@ public class EventAddEditFragment extends Fragment {
     private static Event sEvent;
 
     @BindView(R.id.event_edit_add_save) Button eventSaveButton;
+    @BindView(R.id.event_edit_add_change_image) Button eventChangeImage;
     @BindView(R.id.event_edit_add_name) EditText eventNameEditText;
     @BindView(R.id.event_edit_add_desc) EditText eventDescEditText;
     @BindView(R.id.event_edit_add_image) ImageView eventImageView;
@@ -100,5 +107,45 @@ public class EventAddEditFragment extends Fragment {
         EventPrefUtil.saveEvent(getActivity(),sEvent.getId()+"",sEvent);
         //Remove this fragment from the backstack
         FragmentNavigationUtil.back(getActivity(), 0);
+    }
+
+    //TODO: Figure this out
+    private final int SELECT_PHOTO = 1;
+    /**
+     * Change the event's image
+     * @param v
+     */
+    @OnClick(R.id.event_edit_add_change_image) void changeImage(View v){
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+    }
+
+    /**
+     * Callback
+     * @param requestCode
+     * @param resultCode
+     * @param imageReturnedIntent
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+                    final Uri imageUri = imageReturnedIntent.getData();
+                    //Set the URI to the event's image field
+                    sEvent.setImage(imageUri.toString());
+                    //Load the image
+                    Glide.with(getActivity())
+                            .load(sEvent.getImage())
+                            .apply(new RequestOptions().centerCrop())
+                            .into(eventImageView);
+
+                    Log.v(TAG, imageUri.toString());
+                }
+                break;
+        }
     }
 }
