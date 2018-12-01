4b4e44125144a2eeefbf0952fc5628221b5a1f43
Norbert Fejer <norbertfejer@protonmail.com>
2018-12-01 11:34:24 +0200

    registration added

---
+++ app/src/main/java/szamtech/fejer_patka/ms/sapientia/ro/sapivents/utils/FirebaseAuthUtil.java
@@ -0,0 +1,195 @@
+package szamtech.fejer_patka.ms.sapientia.ro.sapivents.utils;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import szamtech.fejer_patka.ms.sapientia.ro.sapivents.fragments.event.EventListFragment;
+import szamtech.fejer_patka.ms.sapientia.ro.sapivents.fragments.user.UserRegistrationFragment;
+
+public class FirebaseAuthUtils {
+
+    private static final String TAG = "FIREBASE_AUTH_UTILS";
+    private final Context mContext;
+
+    private boolean mVerificationInProgress = false;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    private FirebaseAuth mAuth;
+
+    public FirebaseAuthUtils(Context context) {
+        mContext = context;
+    }
+
+    public void initializeFirebaseAuth(){
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void createCallback() {
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+                Log.d(TAG, "onVerificationCompleted:" + credential);
+                mVerificationInProgress = false;
+
+                Dialog dialog = verificationDialog();
+                dialog.show();
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Log.w(TAG, "onVerificationFailed", e);
+
+                mVerificationInProgress = false;
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                    Toast.makeText(mContext,"Invalid request!",Toast.LENGTH_SHORT).show();
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                    Toast.makeText(mContext,"The SMS quota for the project has been exceeded!",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+                Log.d(TAG, "onCodeSent:" + verificationId);
+
+                // Save verification ID and resending token so we can use them later
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+            }
+        };
+    }
+
+    private void startPhoneNumberVerification(String phoneNumber) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                30L,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                (Activity) mContext,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+
+        mVerificationInProgress = true;
+    }
+
+    private void verifyPhoneNumberWithCode(String verificationId, String code) {
+
+        Log.d(TAG, "verify: " + code);
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener((Activity) mContext, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            if(task.getResult().getAdditionalUserInfo().isNewUser()){
+                                Log.d(TAG, "New unregistered user");
+                                UserRegistrationFragment userRegistrationFragment = new UserRegistrationFragment();
+                                FragmentNavigationUtil.addFragmentOnTop(mContext, userRegistrationFragment, szamtech.fejer_patka.ms.sapientia.ro.sapivents.R.id.fragment_place_signin);
+                            }
+                            else{
+                                if(user.getDisplayName() == "") {
+                                    Log.d(TAG, "New registered user");
+                                    UserRegistrationFragment userRegistrationFragment = new UserRegistrationFragment();
+                                    FragmentNavigationUtil.addFragmentOnTop(mContext, userRegistrationFragment, szamtech.fejer_patka.ms.sapientia.ro.sapivents.R.id.fragment_place_signin);
+                                }
+                                else {
+                                    Log.d(TAG, "Existing user");
+                                    EventListFragment eventListFragment = new EventListFragment();
+                                    FragmentNavigationUtil.addFragmentOnTop(mContext, eventListFragment, szamtech.fejer_patka.ms.sapientia.ro.sapivents.R.id.fragment_place_signin);
+                                }
+                            }
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure");
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                Toast.makeText(mContext,"The verification code entered was invalid!",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    public Dialog verificationDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        // Get the layout inflater
+        LayoutInflater inflater = ((Activity)mContext).getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        final View view = inflater.inflate(szamtech.fejer_patka.ms.sapientia.ro.sapivents.R.layout.sign_in_dialog, null);
+        builder.setView(view)
+                // Add action buttons
+                .setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.w(TAG, "Sign In btn pressed in sign in dialog");
+
+                        EditText editText = (EditText) view.findViewById(szamtech.fejer_patka.ms.sapientia.ro.sapivents.R.id.sign_in_dialog_validation_code);
+                        Log.w(TAG, "Ver code from edittext: " + editText.getText().toString());
+                        verifyPhoneNumberWithCode(mVerificationId, editText.getText().toString());
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.w(TAG, "Cancel btn pressed in sign in dialog");
+                    }
+                });
+        return builder.create();
+    }
+
+    private void resendVerificationCode(String phoneNumber,
+                                        PhoneAuthProvider.ForceResendingToken token) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                (Activity) mContext,               // Activity (for callback binding)
+                mCallbacks,         // OnVerificationStateChangedCallbacks
+                token);             // ForceResendingToken from callbacks
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+    }
+}
